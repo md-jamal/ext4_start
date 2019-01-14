e36870f2189bf28757e2772fab59588ee156a8c9
root <root@u.saraboju@globaledgesoft.com>
2019-01-14 18:20:54 +0530

    more robust file content print
    Signed-off-by: root <root@u.saraboju@globaledgesoft.com>

--- content_ex.c
+++ content_ex.c
@@ -1,5 +1,8 @@
 #include "ext4.h"
 
+unsigned long long file_size = 0;
+unsigned long long cur_size = 0;
+
 int get_gd_count (int fd)
 {
 	struct stat dev;
@@ -24,7 +27,11 @@ int get_gd_count (int fd)
 void hexdump(char *buf, int size)
 {
 	int index = 0 ;
-	//printf("entered %d\n", size);
+#if 0
+	printf("entered %d\n", size);
+	printf("%u %u|n", file_size, cur_size);
+	return ;
+#endif
 	for (; size ; size--)
 		printf("%c",*buf++);
 }
@@ -39,9 +46,17 @@ void print_content(int f, int length, unsigned long long off)
 	
 	int j = 0 ;
 
-	for(; j < length ; j++) {
-			read(f, buffer, 4096);
-			hexdump(buffer, 4096);
+	int length_1 = 0; 
+
+	for(; j<length; j++) {
+	if ( (file_size-cur_size) > 4096)
+			length_1 = 4096;
+	else
+		length_1 = file_size % 4096 ;
+		
+			read(f, buffer, length_1);
+			hexdump(buffer, length_1);
+			cur_size+=length_1;
 	}
 
 	lseek(f, _off, SEEK_SET);
@@ -80,7 +95,6 @@ void parse_ex_tree(int f, unsigned long long off)
 			printf ("block    %d\n",     ex.ee_block);
 			printf ("len      %d\n", 	 ex.ee_len);
 			printf ("start %d\n", 	 ex.ee_start_lo | (ex.ee_start_hi << 32));
-#endif
 			if ( ex.ee_block != ex.ee_block+ex.ee_len-1 )
 				printf ("(%d-%d):%d-%d, ",ex.ee_block, ex.ee_block+ex.ee_len-1, 
 						ex.ee_start_lo | (ex.ee_start_hi << 32),
@@ -88,6 +102,7 @@ void parse_ex_tree(int f, unsigned long long off)
 			else
 				printf ("(%d):%d, ",ex.ee_block, 
 						ex.ee_start_lo | (ex.ee_start_hi << 32));
+#endif
 			
 			print_content(f, ex.ee_len, ex.ee_start_lo | (ex.ee_start_hi << 32));
 		}
@@ -105,7 +120,7 @@ void parse_ex_tree(int f, unsigned long long off)
 			printf ("leaf      %d\n", 	   ehid.ei_leaf_hi);
 			printf ("unused     %d\n",     ehid.ei_unused);
 #endif
-			printf ("(ETB0):%d, ",      ehid.ei_leaf_lo| (ehid.ei_leaf_hi <<32));
+			//printf ("(ETB0):%d, ",      ehid.ei_leaf_lo| (ehid.ei_leaf_hi <<32));
 			parse_ex_tree( f, ehid.ei_leaf_lo| (ehid.ei_leaf_hi <<32) );
 		}
 
@@ -185,8 +200,6 @@ int main (int argc, char *argv[])
 
 #define offsetof(TYPE, MEMBER)  ((size_t)&((TYPE *)0)->MEMBER)
 
-	printf ("offset is %d\n", offsetof(struct ext4_inode, i_block));
-
 	unsigned long long _off = read(f, 0, SEEK_CUR);
 
 	read(f, &ext4_inode, sizeof (struct ext4_inode));
@@ -203,17 +216,19 @@ int main (int argc, char *argv[])
 
 #if 1
 		int loop_index = 0 ;
-
+#if 0
 		printf ("Iblock %u\n", ext4_inode.i_block);
 
 		for (index=0; index < EXT4_N_BLOCKS ; index++) {
 			printf (" block pointer. %d = %d\n", index, ext4_inode.i_block[index]);
 		}
+#endif
 		lseek(f, -sizeof (struct ext4_inode), SEEK_CUR);
 		lseek(f, offsetof(struct ext4_inode, i_block), SEEK_CUR);
 
+		file_size = ((ext4_inode.i_size_high << 32) | ext4_inode.i_size_lo);
 		parse_ex_tree(f, 0);
-		puts("");
+		//puts("");
 
 #endif
 	}
